5820ed6058cf0c415b57e9e512e39e2c15fc3c90
Ivan Herrera <123395168+ivanherreradev@users.noreply.github.com>
2023-03-10 08:35:15 -0300

    Add initial boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes';
+
+const App = () => {
+    return <RouterProvider router={router}></RouterProvider>;
+};
+
+export default App;
