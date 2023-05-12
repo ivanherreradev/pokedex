2d89c2e39013e672567feb19b07516611599ffbd
ivanherreradev <ivanh.developer@gmail.com>
2023-05-12 14:18:54 -0300

    feat: add router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
+import Home from './pages/Home/Home.jsx';
+import PokemonDetail from './pages/PokemonDetail/PokemonDetail';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/pokemon/:id',
+    element: <PokemonDetail />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
